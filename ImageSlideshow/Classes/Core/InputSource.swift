72329c3bd7b3d58fbb91e07ff4448d84554318a0
Colin Goudie <colin.goudie@tributech.com.au>
2024-11-05 17:01:35 +1100

    EMI3 Modifications

--- ImageSlideshow/Classes/Core/InputSource.swift
+++ ImageSlideshow/Classes/Core/InputSource.swift
@@ -17,7 +17,7 @@ import UIKit
      - parameter image: Image that was set to the image view.
      */
     func load(to imageView: UIImageView, with callback: @escaping (_ image: UIImage?) -> Void)
-
+    
     /**
      Cancel image load on the image view
      - parameter imageView: Image view that is loading the image
@@ -29,11 +29,12 @@ import UIKit
 @objcMembers
 open class ImageSource: NSObject, InputSource {
     var image: UIImage
+    var imageTitle: String!
 
     /// Initializes a new Image Source with UIImage
-    /// - parameter image: Image to be loaded
-    public init(image: UIImage) {
+    public init(image: UIImage, imageTitle : String) {
         self.image = image
+        self.imageTitle = imageTitle
     }
 
     /// Initializes a new Image Source with an image name from the main bundle
@@ -48,9 +49,42 @@ open class ImageSource: NSObject, InputSource {
         }
     }
 
-    public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
+    /*public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
         imageView.image = image
         callback(image)
+    }*/
+    
+    public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
+
+        imageView.image = image
+        
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = .yellow
+        label.text = imageTitle
+        
+        label.font = UIFont(name:"Roboto-Bold", size: 14.0)
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.addSubview(label)
+        
+        label.layer.shadowColor = UIColor.black.cgColor
+        label.layer.shadowRadius = 3.0
+        label.layer.shadowOpacity = 1.0
+        label.layer.shadowOffset = CGSize(width: 2, height: 2)
+        label.layer.masksToBounds = false
+        
+        if #available(iOS 9.0, *) {
+            label.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+            label.leftAnchor.constraint(equalTo: imageView.leftAnchor, constant: 5).isActive = true
+            label.rightAnchor.constraint(equalTo: imageView.rightAnchor, constant: 5).isActive = true
+        } else {
+            // Fallback on earlier versions
+        }
+
+        callback(image)
+
     }
 }
 
@@ -58,14 +92,14 @@ open class ImageSource: NSObject, InputSource {
 @objcMembers
 open class BundleImageSource: NSObject, InputSource {
     var imageString: String
-
+    
     /// Initializes a new Image Source with an image name from the main bundle
     /// - parameter imageString: name of the file in the application's main bundle
     public init(imageString: String) {
         self.imageString = imageString
         super.init()
     }
-
+    
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
         let image = UIImage(named: imageString)
         imageView.image = image
@@ -77,14 +111,14 @@ open class BundleImageSource: NSObject, InputSource {
 @objcMembers
 open class FileImageSource: NSObject, InputSource {
     var path: String
-
+    
     /// Initializes a new Image Source with an image name from the main bundle
     /// - parameter imageString: name of the file in the application's main bundle
     public init(path: String) {
         self.path = path
         super.init()
     }
-
+    
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
         let image = UIImage(contentsOfFile: path)
         imageView.image = image
